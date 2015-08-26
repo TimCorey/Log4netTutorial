f7448abe819eca0177c566c51f64e19112efe1f0
Timothy Corey <me@timothycorey.com>
2015-08-26 09:25:46 -0400

    Create Simple Log4net Implementation
    Adds a basic config file setup, the setup code in C# and one call
    to the logger.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch=true)]
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            log.Error("This is my error message");
+
+            Console.ReadLine();
+        }
+    }
+}
