f2b9efdc826ff17f5f0820fd0ba62214eefe7603
Timothy Corey <me@timothycorey.com>
2015-08-28 01:25:06 -0400

    Add Filter Options
    Demonstrated different types of filters and showed how they
    affected the displayed messages.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,13 +10,26 @@ namespace ConsoleUI
 {
     class Program
     {
-        private static readonly log4net.ILog log = LogHelper.GetLogger(); //log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            log.Debug("Developer: Tutorial was run");
+            log.Info("Maintenance: water pump turned on");
+            log.Warn("Maintenance: the water pump is getting hot");
 
-            log.Error("This is my error message");
+            var i = 0;
+
+            try
+            {
+                var x = 10 / i;
+            }
+            catch (DivideByZeroException ex)
+            {
+                log.Error("Developer: we tried to divide by zero again");
+            }
+
+            log.Fatal("Maintenance: water pump exploded");
 
             Console.ReadLine();
         }
